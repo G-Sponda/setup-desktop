ff01575c2b0b8d70b1a497764ffbf86f76441065
Guilherme Sponda <guilhermejsponda@yahoo.com.br>
2018-11-12 08:36:40 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 PKGS_INSTALL = "git dmenu bspwm sxhkd libxcb xcb-util xcb-util-keysyms xcb-util-wm xorg-server thunar termite firefox htop pulseaudio pulseaudio-alsa pavucontrol libmpdclient"
 
 cd $HOME
-sudo pacman -S $PKGS_INSTALL --noconfirm
+sudo pacman -Syu $PKGS_INSTALL --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -16,5 +16,5 @@ cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm/
 chmod +x $HOME/.config/bspwm/bspwmrc
 cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
 
-yay -Sy polybar
+yay -Syu polybar
 install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config

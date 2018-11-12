abaf85d5781528086dac037cedd898ae7964fd8c
Guilherme Sponda <guilhermejsponda@yahoo.com.br>
2018-11-12 08:35:18 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+PKGS_INSTALL = "git dmenu bspwm sxhkd libxcb xcb-util xcb-util-keysyms xcb-util-wm xorg-server thunar termite firefox htop pulseaudio pulseaudio-alsa pavucontrol libmpdclient"
+
 cd $HOME
-sudo pacman -S git dmenu bspwm sxhkd libxcb xcb-util xcb-util-keysyms xcb-util-wm xorg-server thunar termite firefox htop pulseaudio pulseaudio-alsa pavucontrol libmpdclient
+sudo pacman -S $PKGS_INSTALL --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 cd $HOME
-mkdir .config
-cd .config
-mkdir .config/bspwm && mkdir .config/sxhkd
-cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm
+mkdir $HOME/.config
+cd $HOME/.config
+mkdir $HOME/.config/bspwm && mkdir $HOME/.config/sxhkd
+cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm/
 chmod +x $HOME/.config/bspwm/bspwmrc
-cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd
+cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
 
 yay -Sy polybar
 install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
